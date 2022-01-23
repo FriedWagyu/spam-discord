c2613407b206c632a7b06a31e8aadbd8e2b6163f
FriedWagyu <60728947+FriedWagyu@users.noreply.github.com>
2022-01-23 12:37:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pyautogui, time, random
 time.sleep(10)
 pyautogui.FAILSAFE = False
 
-text = open("spam.txt")
+text = open("spam.txt", encoding = "utf-8")
 msg = [x.strip('\n') for x in text]
 
 while True:
 
     ran = random.randint(0, len(msg) - 1)
     randInt = random.randint(120, 180)
-    randSec = randInt / 100
+    randSec = randInt / 1000
     pyautogui.typewrite(msg[ran], randSec)
     time.sleep(randSec)
     pyautogui.press('enter')
