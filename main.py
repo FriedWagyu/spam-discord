e8d8c85aed7986f2367a87ef8d358ed75c50681e
FriedWagyu <60728947+FriedWagyu@users.noreply.github.com>
2022-02-09 15:21:33 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,65 +5,69 @@ import string
 
 
 def creat_random_str():
-    length = random.randint(5, 20)
+    length = random.randint(1, 4)
     sentence = ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
     return sentence
 
 
-def send_random_str():
+def send_str(str_to_send):
     pyautogui.press('space')
-    time.sleep(1)
-    pyautogui.typewrite(creat_random_str(), 1)
-    time.sleep(1)
+    time.sleep(0.1)
+    pyautogui.typewrite(str_to_send, 0.1)
+    time.sleep(0.1)
     pyautogui.press('space')
-    time.sleep(1)
+    time.sleep(0.1)
     pyautogui.press('space')
-    time.sleep(1)
-    pyautogui.press('enter')
-
-
-def send_haha():
-    pyautogui.press('space')
-    time.sleep(1)
-    pyautogui.typewrite('haha', 1)
-    time.sleep(1)
-    pyautogui.press('space')
-    time.sleep(1)
+    time.sleep(0.1)
     pyautogui.press('enter')
 
 
 def delete_last_message():
     pyautogui.press('up')
-    time.sleep(1)
+    time.sleep(0.1)
     pyautogui.hotkey('ctrl', 'a')
-    time.sleep(1)
+    time.sleep(0.1)
     pyautogui.press('backspace')
-    time.sleep(1)
+    time.sleep(0.1)
     pyautogui.press('enter')
-    time.sleep(1)
+    time.sleep(0.1)
     pyautogui.press('enter')
 
 
 def spam_message_loop():
     while True:
-        wait_time = random.randint(240, 480)
-        send_random_str()
-        time.sleep(2)
+        wait_time = random.randint(60, 120)//2
+        send_str(creat_random_str())
+        time.sleep(wait_time)
         delete_last_message()
+        time.sleep(wait_time)
+
+
+def spam_message_loop_no_delete():
+    while True:
+        wait_time = random.randint(300, 600)
+        send_str(creat_random_str())
         time.sleep(wait_time)
 
 
 def spam_haha_loop():
     while True:
-        wait_time = random.randint(240, 480)//2
-        send_haha()
+        wait_time = random.randint(60, 120)//2
+        send_str('haha')
         time.sleep(wait_time)
         delete_last_message()
         time.sleep(wait_time)
 
 
-time.sleep(10)
-pyautogui.FAILSAFE = False
+def delete_all():
+    while True:
+        delete_last_message()
 
-# spam_message_loop()
-spam_haha_loop()
+
+time.sleep(10)
+pyautogui.FAILSAFE = True
+
+spam_message_loop_no_delete()
+#spam_message_loop()
+# spam_haha_loop()
+# delete_all()
